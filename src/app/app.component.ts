156a8d3b5aff30e049c05548befa7e76fe39e8ba
Yarik22 <popov.yarik.popov@gmail.com>
2024-11-27 13:29:27 +0200

    lr14

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,138 +1,64 @@
-import {
-  AfterContentInit,
-  Component,
-  HostListener,
-  OnInit,
-} from '@angular/core';
-import { Router, RouterLink, RouterOutlet } from '@angular/router';
-import { ThemeService } from './services/theme.service';
-import { TranslateModule, TranslateService } from '@ngx-translate/core';
-import { Store } from '@ngrx/store';
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { PageEvent } from '@angular/material/paginator';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTableModule } from '@angular/material/table';
 import { CommonModule } from '@angular/common';
-import { FormsModule, NgForm } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { faker } from '@faker-js/faker';
+interface UserData {
+  id: number;
+  name: string;
+  email: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet,
-    TranslateModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatToolbarModule,
+    MatButtonModule,
     CommonModule,
-    FormsModule,
-    RouterLink,
   ],
-  styleUrl: './app.component.scss',
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit, AfterContentInit {
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
-    const scrollTop =
-      document.documentElement.scrollTop || document.body.scrollTop || 0;
-    this.isScrolled = scrollTop > 100;
-  }
+export class AppComponent implements OnInit {
+  displayedColumns: string[] = ['id', 'name', 'email'];
+  dataSource: MatTableDataSource<UserData> = new MatTableDataSource<UserData>();
+  totalRecords = 100;
+  pageSize = 15;
+  isLoading = false;
 
-  @HostListener('window:scroll', [])
-  onWindowScrolled() {
-    this.isScrolled = window.pageYOffset > 200;
-  }
-  isDarkTheme?: boolean;
-  isScrolled: boolean = false;
-  pageIsNotFound: boolean = false;
-  languages = ['ukr', 'en', 'deu', 'ru'];
-  email: string = 'whatchcam@gmail.com';
-  phone: string = '+380 664 367 109';
-  message: string = '';
-  showNotification: boolean = false;
-  notificationMessage: string = 'Copied';
-  subject: string = '';
-
-  constructor(
-    private themeService: ThemeService,
-    private translator: TranslateService,
-    private router: Router
-  ) {}
-  ngAfterContentInit(): void {
-    this.isDarkTheme = this.themeService.theme === 'dark';
-    this.toggleTheme();
-  }
-
-  toggleTheme() {
-    let theme: string = this.isDarkTheme ? 'dark' : 'default';
-    this.isDarkTheme = !this.isDarkTheme;
-    this.themeService.setTheme(theme);
-  }
+  users: UserData[] = [];
 
   ngOnInit(): void {
-    this.onWindowScroll();
-    const defaultLang = localStorage.getItem('lang') || 'en';
-    this.translator.setDefaultLang(defaultLang);
-    this.translator.use(defaultLang);
+    this.loadUsers(0, this.pageSize);
   }
 
-  changeLanguage(lang: string) {
-    this.translator.use(lang);
-    localStorage.setItem('lang', lang);
-  }
-
-  scrollToTop() {
-    window.scrollTo(0, 0);
-  }
-
-  copyToClipboard(text: string) {
-    navigator.clipboard.writeText(text).then(
-      () => {
-        this.showNotificationMessage(`${text} copied to clipboard`);
-      },
-      (err) => {
-        console.error('Could not copy text: ', err);
-      }
-    );
-  }
-
-  showNotificationMessage(message: string) {
-    this.notificationMessage = message;
-    this.showNotification = true;
+  loadUsers(pageIndex: number, pageSize: number): void {
+    this.isLoading = true;
     setTimeout(() => {
-      this.showNotification = false;
-    }, 3000);
+      const start = pageIndex * pageSize;
+      const end = start + pageSize;
+
+      this.users = Array.from({ length: pageSize }, (_, i) => ({
+        id: start + i + 1,
+        name: faker.person.firstName(),
+        email: faker.internet.email(),
+      }));
+
+      this.dataSource.data = this.users;
+      this.isLoading = false;
+    }, 1000);
   }
 
-  isMobileDevice(): boolean {
-    const userAgent = navigator.userAgent.toLowerCase();
-    return (
-      /iphone|ipod|ipad|android/.test(userAgent) ||
-      (navigator.maxTouchPoints && navigator.maxTouchPoints > 2) ||
-      (window.innerWidth < 600 && window.innerHeight < 600)
-    );
-  }
-
-  sendEmail(event: Event) {
-    if (this.isMobileDevice()) {
-      const emailLink = `mailto:${this.email}?subject=${encodeURIComponent(
-        this.subject
-      )}&body=${encodeURIComponent(this.message)}`;
-      window.location.href = emailLink;
-    } else {
-      event.preventDefault();
-      const emailLink = `https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=${
-        this.email
-      }&su=${encodeURIComponent(this.subject)}&body=${encodeURIComponent(
-        this.message
-      )}`;
-      window.open(emailLink, '_blank');
-    }
-  }
-
-  navigateWithParams() {
-    this.router.navigate(['/debug'], {
-      queryParams: {
-        bg: 'black',
-        height: 600,
-        width: 800,
-        color: 'gray',
-        font: 40,
-      },
-    });
+  onPageChange(event: PageEvent): void {
+    this.loadUsers(event.pageIndex, event.pageSize);
   }
 }
